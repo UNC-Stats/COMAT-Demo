bec4746a97db41744bd0b83cc74e5b60e3348a97
Kristin Kang <kristin.kang@unco.edu>
2024-05-15 22:33:32 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -16,7 +16,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.1",
   "title": "My Section Title",
-  "body": " My Section Title  Section Text. Will you update?  "
+  "body": " My Section Title  Section Text.  "
 },
 {
   "id": "sec-advanced-topics",
@@ -24,8 +24,116 @@ var ptx_lunr_docs = [
   "url": "sec-advanced-topics.html",
   "type": "Section",
   "number": "1.2",
-  "title": "My Advanced Section Title",
-  "body": " My Advanced Section Title  Section Text.  "
+  "title": "Which name belongs to the face on the left?",
+  "body": " Which name belongs to the face on the left?   Photograph in the assets directory.   Photograph of .....  Photographs of faces of two men.    Have student submit answer - collect class data.   Facial Prototyping  A study in Psychonomic Bulletin and Review (Lea, Thomas, Lamkin, and Bell, 2007) presented evidence that people use facial prototypes when they encounter different names. Participants were given two faces and asked to identify which one was Tim and which one was Bob. The researchers wrote that their participants “overwhelmingly agreed” on which face belonged to Tim and which face belonged to Bob but did not provide the exact results of their study.     Perform a full hypothesis test to determine whether UNC students, in general, use facial prototypes when they encounter different names. Use a 5% significance level. Let the parameter of interest be the probability that a student would assign the name Tim to the face on the left.   , where is the concatenation operator.   HT -Manually-authored exercise  Perform a full hypothesis test to determine whether UNC students, in general, use facial prototypes when they encounter different names. Use a 5% significance level. Let the parameter of interest be the probability that a student would assign the name Tim to the face on the left.   We're being a bit tricky here...   , where is the concatenation operator.  CI -Manually-authored exercise  Also compute and interpret a 95% confidence interval for the parameter of interest.   We're being a bit tricky here...   , where is the concatenation operator.   Decision CI - Runestone-powered True\/False Question  Based on the confidence interval, we would NOT reject the null hypothesis.   The null value is outside of the CI.   Practical Import - Runestone-powered Matching Problem  Does your confidence interval suggest that the results are practically important? Explain.     Yes, because....    answer specific feedback      Yes, because......    answer specific feedback      No, because...    answer specific feedback      Question does not apply.    Does not apply if results were not statistically significant.     "
+},
+{
+  "id": "BOB-TIM",
+  "level": "2",
+  "url": "sec-advanced-topics.html#BOB-TIM",
+  "type": "Figure",
+  "number": "1.2.1",
+  "title": "",
+  "body": " Photograph in the assets directory.   Photograph of .....  Photographs of faces of two men.   "
+},
+{
+  "id": "sec-advanced-topics-4",
+  "level": "2",
+  "url": "sec-advanced-topics.html#sec-advanced-topics-4",
+  "type": "Investigation",
+  "number": "1.2.1",
+  "title": "Facial Prototyping.",
+  "body": " Facial Prototyping  A study in Psychonomic Bulletin and Review (Lea, Thomas, Lamkin, and Bell, 2007) presented evidence that people use facial prototypes when they encounter different names. Participants were given two faces and asked to identify which one was Tim and which one was Bob. The researchers wrote that their participants “overwhelmingly agreed” on which face belonged to Tim and which face belonged to Bob but did not provide the exact results of their study.  "
+},
+{
+  "id": "sec-advanced-topics-5",
+  "level": "2",
+  "url": "sec-advanced-topics.html#sec-advanced-topics-5",
+  "type": "Example",
+  "number": "1.2.2",
+  "title": "",
+  "body": "  Perform a full hypothesis test to determine whether UNC students, in general, use facial prototypes when they encounter different names. Use a 5% significance level. Let the parameter of interest be the probability that a student would assign the name Tim to the face on the left.   , where is the concatenation operator.  "
+},
+{
+  "id": "sec-advanced-topics-6",
+  "level": "2",
+  "url": "sec-advanced-topics.html#sec-advanced-topics-6",
+  "type": "Checkpoint",
+  "number": "1.2.3",
+  "title": "HT -Manually-authored exercise.",
+  "body": "HT -Manually-authored exercise  Perform a full hypothesis test to determine whether UNC students, in general, use facial prototypes when they encounter different names. Use a 5% significance level. Let the parameter of interest be the probability that a student would assign the name Tim to the face on the left.  "
+},
+{
+  "id": "sec-advanced-topics-9",
+  "level": "2",
+  "url": "sec-advanced-topics.html#sec-advanced-topics-9",
+  "type": "Checkpoint",
+  "number": "1.2.4",
+  "title": "CI -Manually-authored exercise.",
+  "body": "CI -Manually-authored exercise  Also compute and interpret a 95% confidence interval for the parameter of interest.  "
+},
+{
+  "id": "true-false",
+  "level": "2",
+  "url": "sec-advanced-topics.html#true-false",
+  "type": "Checkpoint",
+  "number": "1.2.5",
+  "title": "Decision CI - Runestone-powered True\/False Question.",
+  "body": "Decision CI - Runestone-powered True\/False Question  Based on the confidence interval, we would NOT reject the null hypothesis.   The null value is outside of the CI.  "
+},
+{
+  "id": "some-matching",
+  "level": "2",
+  "url": "sec-advanced-topics.html#some-matching",
+  "type": "Checkpoint",
+  "number": "1.2.6",
+  "title": "Practical Import - Runestone-powered Matching Problem.",
+  "body": "Practical Import - Runestone-powered Matching Problem  Does your confidence interval suggest that the results are practically important? Explain.     Yes, because....    answer specific feedback      Yes, because......    answer specific feedback      No, because...    answer specific feedback      Question does not apply.    Does not apply if results were not statistically significant.    "
+},
+{
+  "id": "sec-new",
+  "level": "1",
+  "url": "sec-new.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "New Section Title",
+  "body": " New Section Title  Section Text. Inline LaTeX: . Math, newline, centered display is like this!  This is a quote . I want to emphasize the list I'm about to make.   List item 1!    An item with an unordered list.   Item.    Another item.          A population is the entire set of cases that we are interested in studying.      This sample is not representative.     Left for student.    This is a note to the reader.   "
+},
+{
+  "id": "def-population",
+  "level": "2",
+  "url": "sec-new.html#def-population",
+  "type": "Definition",
+  "number": "2.1.1",
+  "title": "",
+  "body": "  A population is the entire set of cases that we are interested in studying.   "
+},
+{
+  "id": "claim-",
+  "level": "2",
+  "url": "sec-new.html#claim-",
+  "type": "Claim",
+  "number": "2.1.2",
+  "title": "",
+  "body": "  This sample is not representative.   "
+},
+{
+  "id": "sec-new-6",
+  "level": "2",
+  "url": "sec-new.html#sec-new-6",
+  "type": "Proof",
+  "number": "2.1.1",
+  "title": "",
+  "body": " Left for student.  "
+},
+{
+  "id": "sec-new-7",
+  "level": "2",
+  "url": "sec-new.html#sec-new-7",
+  "type": "Note",
+  "number": "2.1.3",
+  "title": "",
+  "body": " This is a note to the reader.  "
 },
 {
   "id": "backmatter-2",
