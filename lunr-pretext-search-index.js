407f0c0dcc3b7ed2a0ece550f790390a5988aad8
Kristin Kang <kristin.kang@unco.edu>
2024-05-16 16:24:26 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -24,49 +24,49 @@ var ptx_lunr_docs = [
   "url": "sec-advanced-topics.html",
   "type": "Section",
   "number": "1.2",
-  "title": "Which name belongs to the face on the left?",
-  "body": " Which name belongs to the face on the left?   Photograph in the assets directory.   Photograph of .....  Photographs of faces of two men.    Have student submit answer - collect class data.   Facial Prototyping  A study in Psychonomic Bulletin and Review (Lea, Thomas, Lamkin, and Bell, 2007) presented evidence that people use facial prototypes when they encounter different names. Participants were given two faces and asked to identify which one was Tim and which one was Bob. The researchers wrote that their participants “overwhelmingly agreed” on which face belonged to Tim and which face belonged to Bob but did not provide the exact results of their study.     Perform a full hypothesis test to determine whether UNC students, in general, use facial prototypes when they encounter different names. Use a 5% significance level. Let the parameter of interest be the probability that a student would assign the name Tim to the face on the left.   , where is the concatenation operator.   HT -Manually-authored exercise  Perform a full hypothesis test to determine whether UNC students, in general, use facial prototypes when they encounter different names. Use a 5% significance level. Let the parameter of interest be the probability that a student would assign the name Tim to the face on the left.   We're being a bit tricky here...   , where is the concatenation operator.  CI -Manually-authored exercise  Also compute and interpret a 95% confidence interval for the parameter of interest.   We're being a bit tricky here...   , where is the concatenation operator.   Decision CI - Runestone-powered True\/False Question  Based on the confidence interval, we would NOT reject the null hypothesis.   The null value is outside of the CI.   Practical Import - Runestone-powered Matching Problem  Does your confidence interval suggest that the results are practically important? Explain.     Yes, because....    answer specific feedback      Yes, because......    answer specific feedback      No, because...    answer specific feedback      Question does not apply.    Does not apply if results were not statistically significant.     "
+  "title": "Hypothesis Test",
+  "body": " Hypothesis Test   Which name belongs to the face on the left?      Photograph in the assets directory.   Photograph of .....  Photographs of faces of two men.    Have student submit answer - collect class data.  A printable version (worksheet) of this invetigation, plus questions, is available here: Facial Prototyping .  *Note that this file is stored in your assets folder, and is automatically put in external when pretext build the file.   Facial Prototyping  A study in Psychonomic Bulletin and Review (Lea, Thomas, Lamkin, and Bell, 2007) presented evidence that people use facial prototypes when they encounter different names. Participants were given two faces and asked to identify which one was Tim and which one was Bob. The researchers wrote that their participants “overwhelmingly agreed” on which face belonged to Tim and which face belonged to Bob but did not provide the exact results of their study.  Use StatKey to answer the first two checkpoint questions below. Click here to access the actual article.     HT -Manually-authored exercise  Perform a full hypothesis test to determine whether UNC students, in general, use facial prototypes when they encounter different names. Use a 5% significance level. Let the parameter of interest be the probability that a student would assign the name Tim to the face on the left.   We're being a bit tricky here...   , where is the concatenation operator.    CI -Manually-authored exercise  Also compute and interpret a 95% confidence interval for the parameter of interest.   We're being a bit tricky here...   , where is the concatenation operator.     Decision CI - Runestone-powered True\/False Question  Based on the confidence interval, we would NOT reject the null hypothesis.   The null value is outside of the CI.     Practical Import - Runestone-powered Matching Problem  Does your confidence interval suggest that the results are practically important? Explain.     Yes, because....    answer specific feedback      Yes, because......    answer specific feedback      No, because...    answer specific feedback      Question does not apply.    Does not apply if results were not statistically significant.     The video below would be a demo of one of us using StatKey.   StatKey for the Hypothesis Test  Hello World    "
 },
 {
-  "id": "BOB-TIM",
+  "id": "sec-advanced-topics-2",
   "level": "2",
-  "url": "sec-advanced-topics.html#BOB-TIM",
+  "url": "sec-advanced-topics.html#sec-advanced-topics-2",
+  "type": "Investigation",
+  "number": "1.2.1",
+  "title": "Which name belongs to the face on the left?",
+  "body": " Which name belongs to the face on the left?    "
+},
+{
+  "id": "BOBorTIM",
+  "level": "2",
+  "url": "sec-advanced-topics.html#BOBorTIM",
   "type": "Figure",
   "number": "1.2.1",
   "title": "",
   "body": " Photograph in the assets directory.   Photograph of .....  Photographs of faces of two men.   "
 },
 {
-  "id": "sec-advanced-topics-4",
+  "id": "sec-advanced-topics-7",
   "level": "2",
-  "url": "sec-advanced-topics.html#sec-advanced-topics-4",
-  "type": "Investigation",
-  "number": "1.2.1",
-  "title": "Facial Prototyping.",
-  "body": " Facial Prototyping  A study in Psychonomic Bulletin and Review (Lea, Thomas, Lamkin, and Bell, 2007) presented evidence that people use facial prototypes when they encounter different names. Participants were given two faces and asked to identify which one was Tim and which one was Bob. The researchers wrote that their participants “overwhelmingly agreed” on which face belonged to Tim and which face belonged to Bob but did not provide the exact results of their study.  "
-},
-{
-  "id": "sec-advanced-topics-5",
-  "level": "2",
-  "url": "sec-advanced-topics.html#sec-advanced-topics-5",
+  "url": "sec-advanced-topics.html#sec-advanced-topics-7",
   "type": "Example",
   "number": "1.2.2",
-  "title": "",
-  "body": "  Perform a full hypothesis test to determine whether UNC students, in general, use facial prototypes when they encounter different names. Use a 5% significance level. Let the parameter of interest be the probability that a student would assign the name Tim to the face on the left.   , where is the concatenation operator.  "
+  "title": "Facial Prototyping.",
+  "body": " Facial Prototyping  A study in Psychonomic Bulletin and Review (Lea, Thomas, Lamkin, and Bell, 2007) presented evidence that people use facial prototypes when they encounter different names. Participants were given two faces and asked to identify which one was Tim and which one was Bob. The researchers wrote that their participants “overwhelmingly agreed” on which face belonged to Tim and which face belonged to Bob but did not provide the exact results of their study.  Use StatKey to answer the first two checkpoint questions below. Click here to access the actual article.  "
 },
 {
-  "id": "sec-advanced-topics-6",
+  "id": "sec-advanced-topics-9",
   "level": "2",
-  "url": "sec-advanced-topics.html#sec-advanced-topics-6",
+  "url": "sec-advanced-topics.html#sec-advanced-topics-9",
   "type": "Checkpoint",
   "number": "1.2.3",
   "title": "HT -Manually-authored exercise.",
   "body": "HT -Manually-authored exercise  Perform a full hypothesis test to determine whether UNC students, in general, use facial prototypes when they encounter different names. Use a 5% significance level. Let the parameter of interest be the probability that a student would assign the name Tim to the face on the left.  "
 },
 {
-  "id": "sec-advanced-topics-9",
+  "id": "sec-advanced-topics-13",
   "level": "2",
-  "url": "sec-advanced-topics.html#sec-advanced-topics-9",
+  "url": "sec-advanced-topics.html#sec-advanced-topics-13",
   "type": "Checkpoint",
   "number": "1.2.4",
   "title": "CI -Manually-authored exercise.",
@@ -89,6 +89,15 @@ var ptx_lunr_docs = [
   "number": "1.2.6",
   "title": "Practical Import - Runestone-powered Matching Problem.",
   "body": "Practical Import - Runestone-powered Matching Problem  Does your confidence interval suggest that the results are practically important? Explain.     Yes, because....    answer specific feedback      Yes, because......    answer specific feedback      No, because...    answer specific feedback      Question does not apply.    Does not apply if results were not statistically significant.    "
+},
+{
+  "id": "sec-advanced-topics-21",
+  "level": "2",
+  "url": "sec-advanced-topics.html#sec-advanced-topics-21",
+  "type": "Figure",
+  "number": "1.2.7",
+  "title": "StatKey for the Hypothesis Test",
+  "body": " StatKey for the Hypothesis Test  Hello World   "
 },
 {
   "id": "sec-new",
